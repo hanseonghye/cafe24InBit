49a73223d79154bf1ba4dd2c9c8c7ed14abb2db2
hanseonghye <hans12312@naver.com>
2019-06-14 17:37:02 +0900

    change 연습문제3-ex05

--- python_ex03/ex05.py
+++ python_ex03/ex05.py
@@ -1,10 +1,10 @@
 s = [5, 9, 3, 8, 60, 20, 1]
 
-for i in range(len(s)):
-    for j in range(len(s)):
-        if s[i] > s[j]:
-            temp = s[i]
-            s[i] = s[j]
-            s[j] = temp
+print ("before : ",s)
 
-print(s)
+for i in range(len(s)):
+    for j in range(len(s)-1):
+        if s[j] < s[j+1]:
+            s[j], s[j+1] = s[j+1], s[j]
+
+print("after : ",s)
